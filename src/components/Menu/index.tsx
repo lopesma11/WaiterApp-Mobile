22854c0adad4409d749bc87e04fab404701f1c4d
lopesma11 <ml722090@gmail.com>
2025-07-05 10:32:22 -0300

    feat: created ProductModal component and working on it

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -11,67 +11,52 @@ import {
 } from "./styles";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
+import { useState } from "react";
 
 export function Menu() {
-    function getImageUri(imagePath: string) {
-        const baseUrl = "http://192.168.0.15:3001";
-        return `${baseUrl}/uploads/${imagePath}`;
-    }
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
     return (
-        <FlatList
-            data={products}
-            style={{ marginTop: 32 }}
-            contentContainerStyle={{ paddingHorizontal: 24 }}
-            keyExtractor={(product) => product._id}
-            ItemSeparatorComponent={Separator}
-            renderItem={({ item: product }) => (
-                <Product>
-                    <ProductImage
-                        source={{
-                            uri: `http://192.168.0.15:3001/uploads/${product.imagePath}`,
-                        }}
-                    />
+        <>
+            <ProductModal
+                visible={isModalVisible}
+                onClose={() => setIsModalVisible(false)}
+            ></ProductModal>
+            <FlatList
+                data={products}
+                style={{ marginTop: 32 }}
+                contentContainerStyle={{ paddingHorizontal: 24 }}
+                keyExtractor={(product) => product._id}
+                ItemSeparatorComponent={Separator}
+                renderItem={({ item: product }) => (
+                    <Product onPress={() => setIsModalVisible(true)}>
+                        <ProductImage
+                            source={{
+                                uri: `http://192.168.0.15:3001/uploads/${product.imagePath}`,
+                            }}
+                        />
 
-                    {/* <ProductImage
-                        source={{
-                            uri: getImageUri(product.imagePath),
-                        }}
-                        // Adicionar props para debug e fallback
-                        onError={(error: any) => {
-                            console.log(
-                                "Erro ao carregar imagem:",
-                                error.nativeEvent.error
-                            );
-                        }}
-                        onLoad={() => {
-                            console.log(
-                                "Imagem carregada com sucesso:",
-                                product.name
-                            );
-                        }}
-                        // Adicionar resizeMode para melhor exibição
-                        resizeMode="cover"
-                    /> */}
+                        <ProductDetails>
+                            <Text weight="600">{product.name}</Text>
+                            <Text
+                                size={14}
+                                color="#666"
+                                style={{ marginVertical: 8 }}
+                            >
+                                {product.description}
+                            </Text>
+                            <Text size={14} weight="600">
+                                {formatCurrency(product.price)}
+                            </Text>
+                        </ProductDetails>
 
-                    <ProductDetails>
-                        <Text weight="600">{product.name}</Text>
-                        <Text
-                            size={14}
-                            color="#666"
-                            style={{ marginVertical: 8 }}
-                        >
-                            {product.description}
-                        </Text>
-                        <Text size={14} weight="600">
-                            {formatCurrency(product.price)}
-                        </Text>
-                    </ProductDetails>
-
-                    <AddToCartButton>
-                        <PlusCircle />
-                    </AddToCartButton>
-                </Product>
-            )}
-        />
+                        <AddToCartButton>
+                            <PlusCircle />
+                        </AddToCartButton>
+                    </Product>
+                )}
+            />
+        </>
     );
 }
