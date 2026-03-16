20779b5224acd75ecbc431043e09c3f9569e52c1
MATHEUS  MANOEL <MANOEM1@sa.ad.whirlpool.com>
2026-03-16 14:45:01 -0300

    feat(mobile): replace hardcoded API URL with environment variable
    - Replace hardcoded IP with EXPO_PUBLIC_API_URL environment variable in axios instance
    - Add .env.example for environment documentation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const api = axios.create({
-    baseURL: "http://192.168.0.15:3001",
+const api = axios.create({
+    baseURL: process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.0.15:3001",
 });
+
+export default api;
